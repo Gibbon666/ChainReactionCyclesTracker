b609e46325c5d35ac892854162f071a881115426
Daniel Baranyai <bdaniel6@gmail.com>
2019-08-30 20:46:59 +0300

    create function and smtp service

--- scraper.py
+++ scraper.py
@@ -2,27 +2,24 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from send_price import *
+import time
 
-URL = 'https://www.chainreactioncycles.com/fi/en/mountain-bikes?f=2258&sort=pricelow'
-headers = {"User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'}
+def check_price():
+    URL = 'https://www.chainreactioncycles.com/fi/en/mountain-bikes?f=2258&sort=pricelow'
+    headers = {"User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'}
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-page = requests.get(URL, headers=headers)
+    prices_raw = soup.find_all("li", class_="fromamt")
+    prices_clean = []
+    for row in prices_raw:
+        prices_clean.append(float(re.findall(r'\d+.\d\d', str(row))[0]))
+        
+    if min(prices_clean) < 300:
+        send_simple_message(min(prices_clean), URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-prices_raw = soup.find_all("li", class_="fromamt")
-prices_clean = []
-for row in prices_raw:
-    prices_clean.append(re.findall(r'€\d+.\d\d', str(row)))
-    
-print(prices_clean)
-
-# print(re.findall('€\d+.\d\d', "€714.49</li>"))
-
-# tag = soup.li
-# print(tag["class"])
-# print(soup.find(id="fromamt").get_text())
-
-
-# print(prices)
+while(True):
+    check_price()
+    time.sleep(3600)
 
